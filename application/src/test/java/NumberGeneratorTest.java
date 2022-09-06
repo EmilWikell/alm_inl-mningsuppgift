8c886081261d1789a958991175a3e40844edcb60
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2022-09-06 14:59:21 +0200

    Philip t more tests (#16)
    * added more tests
    
    * added more tests

--- application/src/test/java/NumberGeneratorTest.java
+++ application/src/test/java/NumberGeneratorTest.java
@@ -13,4 +13,18 @@ class NumberGeneratorTest {
         assertTrue(resultNumber < 10);
         //test comment
     }
+
+    @Test
+    void generateRandomNumberIsZeroTest(){
+        int testNumber = 0;
+        int resultNumber = ng.generateRandomNumber(testNumber);
+        assertEquals(testNumber, resultNumber);
+    }
+
+    @Test
+    void generateNegativeNumberTest(){
+        int testNumber = -20;
+        int resultNumber = ng.generateRandomNumber(testNumber);
+        assertTrue(resultNumber <= 0);
+    }
 }
