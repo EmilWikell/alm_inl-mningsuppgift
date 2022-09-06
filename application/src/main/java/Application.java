d0579f38f26949fa40886f536d7ff52533900709
EmilWikell <91671600+EmilWikell@users.noreply.github.com>
2022-09-06 17:55:23 +0200

    changes to pom to make docker image work

--- application/src/main/java/Application.java
+++ application/src/main/java/Application.java
@@ -5,7 +5,7 @@ public class Application {
     public static void main(String[] args) {
 
         NumberGenerator ng = new NumberGenerator();
-        int floorNumber = Integer.parseInt(args[0]);
+        int floorNumber = 100;
         int generatedNumber = ng.generateRandomNumber(floorNumber);
 
         System.out.println(generatedNumber);
